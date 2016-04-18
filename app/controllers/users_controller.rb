3809ba85921d0640154e82f501664f88fc5e34c5
TimBek2 <tim.beck46@gmail.com>
2016-04-18 11:17:36 -0400

    Add New sessions route(for login), Create new User form page, add user_params method for strong parameters to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,32 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(params[:user])
+			if @user.save
+				flash.notice = 'Thanks for registering!'
+				redirect_to user_path(@user)
+			else
+				flash.now[:alert] = 'Something went wrong!'
+				render :new
+			end
 	end
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def destroy
+		@user = User.find(params[:id])
 	end
 
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
