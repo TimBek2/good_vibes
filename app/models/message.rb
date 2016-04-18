badfb9ef11a361e0a12b9a91bf55dedbe1e4fb0e
TimBek2 <tim.beck46@gmail.com>
2016-04-18 06:55:37 -0400

    Delete rails test folder, add canary test to User model

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,11 +5,12 @@ class Message < ActiveRecord::Base
   validates_presence_of :body, :sender_id, :receiver_id
   validates_uniqueness_of :sender_id, scope: :receiver_id
 
-  scope :involving, -> (user) do
-    where("messages.sender_id =? OR messages.receiver_id =?",user.id,user.id)
-  end
+	scope :between, -> (sender_id,receiver_id) do
+		where(“(messages.sender_id = ? AND messages.receiver_id =?) OR (messages.sender_id = ? AND messages.receiver_id =?)”, sender_id,receiver_id, receiver_id, sender_id)
+	end
 
-  scope :between, -> (sender_id,receiver_id) do
-    where("(messages.sender_id = ? AND messages.receiver_id =?) OR (messages.sender_id = ? AND messages.receiver_id =?)", sender_id,receiver_id, receiver_id, sender_id)
-  end
+
+	def message_time
+  	created_at.strftime(“%m/%d/%y at %l:%M %p”)
+	end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User do
+	it "passes the canary test" do
+		expect(true).to be true
+	end
+
+
+
+end
